e36c296ca530ddd0805e2ee5990b7898e86fe77a
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-08-08 23:39:30 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import React from "react";
+
+export const metadata = {
+  title: "Next.js 14 with Clerk",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
